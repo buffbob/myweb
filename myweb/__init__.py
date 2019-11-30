02b75472e1841643cc098b83365b2c6891dc615d
BuffBob <lastgulch@gmail.com>
2019-11-30 11:48:47 -0700

    app factory and blueprints

---
+++ myweb/__init__.py
@@ -0,0 +1,28 @@
+import os
+from flask import Flask
+from myweb.config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+# db = SQLAlchemy()
+# bcrypt = Bcrypt()
+# login_manager = LoginManager()
+#login_manager.login_view = "booger"
+
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    if test_config is None:
+        app.config.from_object(Config)
+    else:
+        app.config.from_mapping(test_config)
+
+    # db.init_app(app)
+    # bcrypt.init_app(app)
+    # login_manager.init_app(app)
+
+    from myweb.users.routes import users
+
+    app.register_blueprint(users)
+
+    return app
---
+++ myweb/config.py
@@ -0,0 +1,9 @@
+import os
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY")
+    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    SQLALCHEMY_ECHO = True
+
+
+# #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
