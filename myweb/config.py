7647c5ce76f20811b9956371f312a53314761c13
BuffBob <lastgulch@gmail.com>
2020-01-02 20:04:52 -0700

    back to working, tweaked config.py

--- myweb/config.py
+++ myweb/config.py
@@ -1,9 +1,11 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
-    SQLALCHEMY_ECHO = True
+    SECRET_KEY=os.environ.get("SECRET_KEY")
+    # SQLALCHEMY_DATABASE_URI = os.getenv("SQLALCHEMY_DATABASE_URI")
+    # SQLALCHEMY_ECHO = True
+    FLASK_APP=os.environ.get("FLASK_APP")
+    FLASK_ENV=os.environ.get("FLASK_ENV")
+    #Later when moveing to production will use the included json file for variables such as these
 
 
-# #app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
